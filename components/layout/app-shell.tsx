37523be013bcc01b0ecff0cae24272b72846ef0c
futurestands <mugarurajohnson56@gmail.com>
2026-08-06 20:14:52 +0300

    receive

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -166,8 +166,8 @@ export function AppShell({
             </Link>
           </div>
 
-          <div className="hidden md:flex items-center flex-1 px-8 min-w-0 justify-center">
-            <p className="text-[10px] md:text-xs lg:text-sm xl:text-base font-black text-brand-blue tracking-[0.1em] text-center font-serif uppercase leading-tight">
+          <div className="hidden md:flex items-center flex-1 px-4 min-w-0 justify-center">
+            <p className="text-[10px] md:text-sm lg:text-lg xl:text-xl font-black text-brand-blue tracking-[0.2em] text-center font-serif uppercase whitespace-nowrap">
               {orgName}
             </p>
           </div>
