680604d118b95cd3c11af8e8914a5752c0e40c28
futurestands <mugarurajohnson56@gmail.com>
2026-09-10 16:52:07 +0300

    receive

--- android/app/src/main/java/org/swuws/portal/MainActivity.java
+++ android/app/src/main/java/org/swuws/portal/MainActivity.java
@@ -2,10 +2,12 @@ package org.swuws.portal;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.database.sqlite.CapacitorSQLitePlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        registerPlugin(CapacitorSQLitePlugin.class);
     }
 }
